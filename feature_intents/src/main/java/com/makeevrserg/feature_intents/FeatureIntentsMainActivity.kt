21ccfe0597e8d3545832fb11c3421c1be7ef9ca0
Roman Makeev <makeevrserg@gmail.com>
2022-07-23 12:43:09 +0300

    Dark/Light mode

--- feature_intents/src/main/java/com/makeevrserg/feature_intents/FeatureIntentsMainActivity.kt
+++ feature_intents/src/main/java/com/makeevrserg/feature_intents/FeatureIntentsMainActivity.kt
@@ -1,11 +1,14 @@
 package com.makeevrserg.feature_intents
 
+import BaseActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.makeevrserg.feature_intents.databinding.FeatureIntentsActivityMainBinding
 
-class FeatureIntentsMainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.feature_intents_activity_main)
+class FeatureIntentsMainActivity :
+    BaseActivity<FeatureIntentsActivityMainBinding>(FeatureIntentsActivityMainBinding::inflate) {
+    override fun setupObservables(binding: FeatureIntentsActivityMainBinding) {
+
     }
+
 }
