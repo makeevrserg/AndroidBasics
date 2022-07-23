87e4bb5d918e40a9997e1f415f8e7ed169ae2694
Roman Makeev <makeevrserg@gmail.com>
2022-07-23 13:09:12 +0300

    URL launch intent

--- feature_intents/src/main/java/com/makeevrserg/feature_intents/FeatureIntentsMainActivity.kt
+++ feature_intents/src/main/java/com/makeevrserg/feature_intents/FeatureIntentsMainActivity.kt
@@ -2,9 +2,14 @@ package com.makeevrserg.feature_intents
 
 import AndroidExtensions.launchActivity
 import BaseActivity
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.makeevrserg.feature_intents.databinding.FeatureIntentsActivityMainBinding
 import java.util.*
 
@@ -29,6 +34,20 @@ class FeatureIntentsMainActivity :
             binding.bLaunchActivity.visibility = View.GONE
             binding.tvTitle.text = getString(R.string.feature_intents_launched)
             binding.tvTitleExtra.text = it
+        }
+        binding.bLaunchUrl.setOnClickListener {
+            try {
+                launchActivity(Intent.ACTION_VIEW, Uri.parse("https://astrainteractive.ru")) {
+                    addCategory(Intent.CATEGORY_BROWSABLE)
+                }
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.no_browser_activity),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
         }
     }
 
--- resources/src/main/java/AndroidExtensions.kt
+++ resources/src/main/java/AndroidExtensions.kt
@@ -1,8 +1,12 @@
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 
 object AndroidExtensions {
     inline fun <reified T : AppCompatActivity> Context.launchActivity(block: Intent.() -> Unit = {}) =
         Intent(this, T::class.java).apply(block).also(::startActivity)
+
+    inline fun Context.launchActivity(action: String, uri: Uri, block: Intent.() -> Unit = {}) =
+        Intent(action, uri).apply(block).also(::startActivity)
 }
