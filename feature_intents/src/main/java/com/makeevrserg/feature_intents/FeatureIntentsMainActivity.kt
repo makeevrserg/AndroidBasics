eda5b00166c3c760573bdbd9ca5cbfa92c9930a0
Roman Makeev <makeevrserg@gmail.com>
2022-07-23 12:58:34 +0300

    Intent put Extra example

--- feature_intents/src/main/java/com/makeevrserg/feature_intents/FeatureIntentsMainActivity.kt
+++ feature_intents/src/main/java/com/makeevrserg/feature_intents/FeatureIntentsMainActivity.kt
@@ -1,14 +1,38 @@
 package com.makeevrserg.feature_intents
 
+import AndroidExtensions.launchActivity
 import BaseActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.makeevrserg.feature_intents.databinding.FeatureIntentsActivityMainBinding
+import java.util.*
 
 class FeatureIntentsMainActivity :
     BaseActivity<FeatureIntentsActivityMainBinding>(FeatureIntentsActivityMainBinding::inflate) {
-    override fun setupObservables(binding: FeatureIntentsActivityMainBinding) {
-
+    private var extraTitle: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        intent?.let {
+            extraTitle = it.getStringExtra(TITLE_EXTRA)
+            binding?.let(::setupObservables)
+        }
     }
 
+    override fun setupObservables(binding: FeatureIntentsActivityMainBinding) {
+        binding.bLaunchActivity.setOnClickListener {
+            launchActivity<FeatureIntentsMainActivity> {
+                this.putExtra(TITLE_EXTRA, UUID.randomUUID().toString())
+            }
+        }
+        extraTitle?.let {
+            binding.bLaunchActivity.visibility = View.GONE
+            binding.tvTitle.text = getString(R.string.feature_intents_launched)
+            binding.tvTitleExtra.text = it
+        }
+    }
+
+    companion object {
+        private const val TITLE_EXTRA = "TITLE_EXTRA"
+    }
 }
--- resources/src/main/java/AndroidExtensions.kt
+++ resources/src/main/java/AndroidExtensions.kt
@@ -3,8 +3,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 object AndroidExtensions {
-    inline fun <reified T : AppCompatActivity> Context.launchActivity(block: Intent.() -> Unit = {}) {
-        val intent = Intent(this, T::class.java).apply(block)
-        startActivity(intent)
-    }
+    inline fun <reified T : AppCompatActivity> Context.launchActivity(block: Intent.() -> Unit = {}) =
+        Intent(this, T::class.java).apply(block).also(::startActivity)
 }
